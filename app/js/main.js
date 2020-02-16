c2570aaf0ea7f83ed0f89491e33c3eb0dd96196b
DmytroSmarasinghe <dima35x@ukr.net>
2020-02-16 23:13:24 +0200

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,21 @@
-$(function(){
+$(function () {
+
+
+
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "15px",
+        spacing: "3px",
+        readOnly: true,
+        ratedFill: "#ffc000",
+    })
+
+    $(".products__inner").slick({
+        dots: false,
+        // arrows: false,
+        appendArrows: $('.products__slider-btn'),
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>', 
+    });
 
 });
