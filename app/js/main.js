604236b74d0ade3e1ac9e6da5853d9db5fc1b5d8
DmytroSmarasinghe <dima35x@ukr.net>
2020-03-02 19:57:55 +0200

    added adaptiv header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -24,5 +24,16 @@ $(function () {
         nextArrow: '<button type="submit" class="slick-btn slick-next"></button>',
         prevArrow: '<button type="submit" class="slick-btn slick-prev"></button>', 
     });
+
+    
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').slideToggle();
+    });
+    
+    $('.header__btn-menu').on('click', function () {
+        $('.header__top-icons').toggleClass('active');
+    });
+
     var mixer = mixitup('.all-products__inner-box');
 });
+ 
