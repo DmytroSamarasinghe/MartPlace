ae6c46da019dbc2cf0072175cabcd24a2f009b82
DmytroSmarasinghe <dima35x@ukr.net>
2020-03-09 09:17:22 +0200

    added category-page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-$(function () { 
+$(function () {
 
     $(".rate-star").rateYo({
         rating: 5,
@@ -9,31 +9,69 @@ $(function () {
     })
 
     $(".products__inner").slick({
-        dots: false, 
+        dots: false,
         appendArrows: $('.products__slider-btn'),
         nextArrow: '<button type="submit" class="slick-btn slick-next"></button>',
-        prevArrow: '<button type="submit" class="slick-btn slick-prev"></button>', 
+        prevArrow: '<button type="submit" class="slick-btn slick-prev"></button>',
     });
 
     $(".bottom-products__inner").slick({
-        dots: false, 
+        dots: false,
         slidesToShow: 3,
-        slidesToScroll: 3,  
-        variableWidth: true, 
+        slidesToScroll: 3,
+        variableWidth: true,
         appendArrows: $('.bottom-products__slider-btn'),
         nextArrow: '<button type="submit" class="slick-btn slick-next"></button>',
-        prevArrow: '<button type="submit" class="slick-btn slick-prev"></button>', 
+        prevArrow: '<button type="submit" class="slick-btn slick-prev"></button>',
     });
 
-    
+
     $('.menu__btn').on('click', function () {
         $('.menu__list').slideToggle();
     });
-    
+
     $('.header__btn-menu').on('click', function () {
         $('.header__top-icons').toggleClass('active');
     });
 
+    $("#rang-slid").ionRangeSlider({
+        skin: "round",
+        type: "double",
+        min: 0,
+        max: 330,
+        from: 30,
+        to: 300,
+        prefix: "$",
+    });
+
+
+    // $('.category-page__products-aside')(function () { 
+    $('.categories>.aside__item-top').on('click', function () {
+        $('.categories>.aside__item-bottom').slideToggle();
+        $('.categories>.aside__item-top').toggleClass('disable'); 
+    });
+    $('.filter-products>.aside__item-top').on('click', function () {
+        $('.filter-products>.aside__item-bottom').slideToggle();
+        $('.filter-products>.aside__item-top').toggleClass('disable');
+    });
+    $('.pricing-range>.aside__item-top').on('click', function () {
+        $('.pricing-range>.aside__item-bottom').slideToggle();
+        $('.pricing-range>.aside__item-top').toggleClass('disable');
+    });
+    // });
+
+    $('.filter-large').on('click', function () {
+        $('.small-products>.products__item').removeClass('list');
+        $('.filter-list').removeClass('active');
+        $('.filter-large').addClass('active');
+    });
+    $('.filter-list').on('click', function () {
+        $('.small-products>.products__item').addClass('list');
+        $('.filter-large').removeClass('active');
+        $('.filter-list').addClass('active');
+    });
+
+    $('input[type="file"], select').styler();
+    
     var mixer = mixitup('.all-products__inner-box');
 });
- 
--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,9 @@ gulp.task('style', function(){
         'node_modules/slick-carousel/slick/slick.css',
         'node_modules/magnific-popup/dist/magnific-popup.css',
         'node_modules/rateyo/src/jquery.rateyo.css', 
+        'node_modules/ion-rangeslider/css/ion.rangeSlider.css', 
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.css',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css',
     ])
     .pipe(concat('libs.min.css'))
     .pipe(cssmin())
@@ -36,6 +39,8 @@ gulp.task('script', function(){
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
         'node_modules/mixitup/dist/mixitup.js',
         'node_modules/rateyo/src/jquery.rateyo.js', 
+        'node_modules/ion-rangeslider/js/ion.rangeSlider.js', 
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
     ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
