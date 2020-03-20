8607fd08e280d2f2d62c4cce927a3a6059f03af1
DmytroSmarasinghe <dima35x@ukr.net>
2020-03-20 18:43:39 +0200

    added debag

--- app/js/main.js
+++ app/js/main.js
@@ -125,7 +125,14 @@ $(function () {
     });
     //blog
 
-
+$('.product-one__tabs .tab, .settins__tabs .tab').on('click', function (event) {	
+        var id = $(this).attr('data-id');	
+        $('.product-one__tabs, .settins__tabs').find('.tab-item').removeClass('active-tab').hide();	
+        $('.product-one__tabs .tabs, .settins__tabs .tabs').find('.tab').removeClass('active');	
+        $(this).addClass('active');	
+        $('#' + id).addClass('active-tab').fadeIn();	
+        return false;	
+    });
 
 
     // 
